16cc7b8f135a5dbca5596eb9a522bd28028e2cb6
greicens <gsilva@mail.ccsf.edu>
2016-12-19 12:19:45 -0800

    required controllers

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var db = require('./models');
+var controllers = require('./controllers');
 
+console.log(controllers);
 var app = express();
 
 //what does this do?
@@ -11,6 +13,7 @@ app.get('/', function (req, res){
   res.sendFile('views/index.html',{root: __dirname});
 });
 
+app.get('/api', controllers.api.index);
 
 app.listen(process.env.PORT || 3000, function(){
   console.log('Express server is up and running on http://localhost:3000/');
