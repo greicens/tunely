334542aa09268adc42d00982ba94def48363089f
ryanthomas92 <rthoma11@villanova.edu>
2016-12-19 15:05:38 -0800

    created ajax functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,31 +7,6 @@
 
 
 /* hard-coded data! */
-var sampleAlbums = [];
-sampleAlbums.push({
-             artistName: 'Ladyhawke',
-             name: 'Ladyhawke',
-             releaseDate: '2008, November 18',
-             genres: [ 'new wave', 'indie rock', 'synth pop' ]
-           });
-sampleAlbums.push({
-             artistName: 'The Knife',
-             name: 'Silent Shout',
-             releaseDate: '2006, February 17',
-             genres: [ 'synth pop', 'electronica', 'experimental' ]
-           });
-sampleAlbums.push({
-             artistName: 'Juno Reactor',
-             name: 'Shango',
-             releaseDate: '2000, October 9',
-             genres: [ 'electronic', 'goa trance', 'tribal house' ]
-           });
-sampleAlbums.push({
-             artistName: 'Philip Wesley',
-             name: 'Dark Night of the Soul',
-             releaseDate: '2008, September 12',
-             genres: [ 'piano' ]
-           });
 /* end of hard-coded data */
 
 
@@ -45,13 +20,26 @@ $(document).ready(function() {
 
   var source = $("#albums-template").html();
   template = Handlebars.compile(source);
-  sampleAlbums.forEach(function(album){
-    renderAlbum(album);
-  })
+
+  $.ajax({
+    method: 'GET',
+    url: '/api/albums',
+    success: onSuccess,
+    error: onError
+  });
 
 });
 
+function onSuccess(json) {
+  json.forEach(function(album) {
+    console.log(album);
+    renderAlbum(album);
+  });
+}
 
+function onError(err) {
+    console.log(err);
+}
 
 
 
