f741d7f9e149e64e449566838c10884b920a6af4
greicens <gsilva@mail.ccsf.edu>
2016-12-20 09:28:06 -0800

    created song schema and require in album.js

--- models/album.js
+++ models/album.js
@@ -1,11 +1,14 @@
 var mongoose = require('mongoose'),
   Schema = mongoose.Schema;
 
+  module.exports.Song = require('./song');
+
 var AlbumSchema = new Schema({
   name: String,
   artistName: String,
   releaseDate: String,
-  genres: [String]
+  genres: [String],
+  songs: [Song.schema]
 });
 
 var Album = mongoose.model('Album', AlbumSchema);
